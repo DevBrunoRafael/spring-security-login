16ca16a03c7defa146076db39de1384b9592f945
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-09-11 23:13:38 -0300

    adding new security settings

--- src/main/java/com/devbrunorafael/springsecuritylogin/security/SecurityConfig.java
+++ src/main/java/com/devbrunorafael/springsecuritylogin/security/SecurityConfig.java
@@ -1,19 +1,32 @@
 package com.devbrunorafael.springsecuritylogin.security;
 
+import com.devbrunorafael.springsecuritylogin.domain.repository.UserRepository;
+import com.devbrunorafael.springsecuritylogin.domain.service.SSUserDetailsService;
+import com.devbrunorafael.springsecuritylogin.domain.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private SSUserDetailsService userDetailsService;
+    private UserService userService;
 
     @Bean
     public static BCryptPasswordEncoder passwordEncoder(){
@@ -22,19 +35,38 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin()
-                .loginPage("/login").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/").access("hasAnyAuthority('USERS', 'ADMIN')")
+                .antMatchers("/admin").access("hasAnyAuthority('ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll()
+                .and()
+                .httpBasic();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
 
         return http.build();
     }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("123"))
-                .authorities("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(passwordEncoder().encode("123"))
+//                .authorities("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("adm")
+//                .password(passwordEncoder().encode("321"))
+//                .authorities("ADMIN")
+//                .build();
+
+        return new InMemoryUserDetailsManager();
     }
 }
